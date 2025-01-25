2b0c00931ec9416fe62eb67d9cc4e31ea7c305e3
”SammiOnlinegit config --global user.name ”SammiOnline <llayey@hotmail.com>
2025-01-25 17:00:50 +0100

    Lägger upp allt till GitHub

--- my-vite-app/vite.config.js
+++ my-vite-app/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/EcomShop/',  // Lägg till denna rad för att anpassa base-URL
+  plugins: [react()], 
 })
